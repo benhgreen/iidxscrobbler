cb517de9bfb5d4c745358aeb02aa9bd5a335cd7e
Ben Green <ben.green@inventati.org>
2014-12-05 18:21:07 -0500

    getting rid of old garbage

--- iidxscrobbler/iidxscrobbler/items.py
+++
@@ -1,9 +0,0 @@
-# Scraping Song ID and Song Title to be scrobbled on last.fm
-# Song ID can access Artist and Genre
-
-from scrapy.item import Item, Field
-
-class IidxscrobblerItem(Item):
-    songtitle = Field()
-    songid = Field()
-    pass
