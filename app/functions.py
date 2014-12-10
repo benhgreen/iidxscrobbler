d397edd49ef8c3978c0c0b3c70e5e652a689a4a4
Ben Green <ben.green@inventati.org>
2014-12-09 20:00:15 -0500

    added support for programmed world

--- app/functions.py
+++ app/functions.py
@@ -10,23 +10,36 @@ songdata = None
 playerdata = None
 
 #scrape data from import.io
-def scrapeData(userid):
+def scrapeData(userid, network):
 	client = clientGen()
 	client.connect()
 	queryLatch = latch.latch(1)
 
-	global target_url, connector_guid
+	global target_url, connector_guid, short_url
 
-	#if the userid is 'refresh_music', this method will update the music library
-	if userid == "refresh_music":
-		print "refreshing songs list..."
-		target_url = "http://webui.programmedsun.com/iidx/0/music"
-		connector_guid = "e53e03d2-1468-4ebb-8fe9-2ef64de33db2"
-	else:
-		print "refreshing player tracklist..."
-		target_url = "http://webui.programmedsun.com/iidx/0/players/%s/scores" % userid
-		connector_guid = "9247219f-a36f-4e6b-85b0-1956eff5836d"
-	
+	#if the userid is 'refresh_music', this method will update the music library, otherwise it will check the user's recently played
+	#stuff for scraping programmed sun
+	if(network == 'ps'):
+		short_url = "webui.programmedsun.com"
+		if userid == "refresh_music":
+			print "refreshing PS song list..."
+			target_url = "http://webui.programmedsun.com/iidx/0/music"
+			connector_guid = "e53e03d2-1468-4ebb-8fe9-2ef64de33db2"
+		else:
+			print "refreshing PS player %s tracklist..." % userid
+			target_url = "http://webui.programmedsun.com/iidx/0/players/%s/scores" % userid
+			connector_guid = "9247219f-a36f-4e6b-85b0-1956eff5836d"
+	#stuff for scraping programmed world
+	elif(network == 'pw'):
+		short_url = "programmedworld.net"
+		if(userid == "refresh_music"):
+			print "refreshing PW song list..."
+			target_url = "https://programmedworld.net/iidx/22/music"
+			connector_guid = "7d120ee9-000f-43f1-961a-17e4ff45771e"
+		else:
+			print "refreshing PW player %s tracklist..." % userid
+			target_url = "https://programmedworld.net/iidx/22/players/%s/scores" % userid
+			connector_guid = "329e12e0-85ea-4961-83b6-a1156e25d46a"
 	#callback to export the returned data
 	def callback(query, message):
 		global data
@@ -36,6 +49,7 @@ def scrapeData(userid):
 		if message["type"] == "MESSAGE":
 			if "errorType" in message["data"]:
 				print "Got an error!" 
+				print json.dumps(message["data"], indent = 4)
 			else:
 				print "Got data!"
 				data = (message["data"]["results"])
@@ -43,6 +57,7 @@ def scrapeData(userid):
 		if query.finished(): 
 			queryLatch.countdown()
 
+	#import.io's template queries sure are awesome
 	client.query({
 		"connectorGuids":[
 			connector_guid
@@ -53,7 +68,7 @@ def scrapeData(userid):
 		"additionalInput": {
 			connector_guid: {
 				"domainCredentials": {
-					"webui.programmedsun.com": {
+					short_url: {
 						"username": getPSuser(),
 						"password": getPSpwd(),
 					}
@@ -69,10 +84,10 @@ def scrapeData(userid):
 
 # refreshes song list and converts to dictionary where key is songid
 # and value is tuple (title, artist)
-def refreshSongList():
+def refreshSongList(network):
 
 	songlist = {'songid' : 'songinfo'}
-	raw_data = scrapeData('refresh_music')
+	raw_data = scrapeData('refresh_music', network)
 
 	for song in raw_data:
 
--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ def iidxScrobble(user, lfm_object):
 	threading.Timer(150.0, iidxScrobble, [user, lfm_object]).start()
 	
 	#get user's last 50 played songs from the server
-	playerlist = scrapeData(user["userid"])
+	playerlist = scrapeData(user["userid"], user["network"])
 
 	#iterate through songs in list
 	for song in playerlist:
@@ -40,7 +40,7 @@ def iidxScrobble(user, lfm_object):
 			(song_name, artist_name) = songLookup(musiclist, stripSongURL(song))
 			submit_time = int((songtime - datetime(1970,1,1)).total_seconds())
 
-			print "scrobbling %s: %s" % (song["song_info/_text"], song["timestamp"])
+			print "		scrobbling %s: %s" % (song["song_info/_text"], song["timestamp"])
 			lfm_object.scrobble(artist=artist_name, title=song_name, timestamp=submit_time)
 
 	
@@ -49,10 +49,10 @@ def iidxScrobble(user, lfm_object):
 if __name__ == '__main__':
 
 	#refresh music list in case there are any new songs
-	musiclist = refreshSongList()
+	musiclist = refreshSongList('pw')
 
 	#fetch user, this would ordinarily be from a database in a web app
-	user = {"userid": "8717-9975", "lastchecked": datetime.strptime("08 Dec 2014 22:42:00", date_format), "lfm_user": LFM_USER, "lfm_pwd": LFM_PWD}
+	user = {"userid": "4623-0106", "lastchecked": datetime.strptime("27 Nov 2014 22:42:00", date_format), "lfm_user": LFM_USER, "lfm_pwd": LFM_PWD, "network": 'pw'}
 
 	#obviously this will already be hashed in the deployed version
 	password_hash = pylast.md5(user["lfm_pwd"])
