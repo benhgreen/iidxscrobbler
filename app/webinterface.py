2d916c8368b6341527b8d370b89a5fabdf5f4ea7
Ben Green <ben.green@inventati.org>
2014-12-15 18:11:25 -0500

    ported to tornado, added bootstrap locally

--- app/webinterface.py
+++ app/webinterface.py
@@ -1,3 +1,7 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
 from flask import Flask, render_template, redirect, flash
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,4 +18,6 @@ def signup():
                            form=form)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+	http_server = HTTPServer(WSGIContainer(app))
+	http_server.listen(5000)
+	IOLoop.instance().start()
